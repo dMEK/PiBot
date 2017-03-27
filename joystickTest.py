601afae6b4f67b24c948f4f34c0968b124e02a62
Dillon MacEwan <dillon@mekazoic.com>
2017-03-27 21:27:35 +1100

    Update joystickTest.py

--- joystickTest.py
+++ joystickTest.py
@@ -6,3 +6,38 @@
 import PDALib2 as hobbit
 import jsLib
 import time
+
+hobbit.RoboPiInit("/dev/ttyAMA0", 115200)
+
+js = jsLib.joystick()
+print "Device is: ",js.Name()
+
+hobbit.motors()
+
+hobbit.pinMode(0, hobbit.SERVO)
+hobbit.servoWrite(0, 1500)
+
+lfrac = 1
+rfrac = 1
+
+while True:
+  got = js.Read()
+  
+  if got[0] == "LY":
+    speed = int(got[1] * -255)
+    hobbit.writeMotors(speed, speed)
+    
+  if got[0] = "RX":
+    steer = got[1]
+    print "Right joystick X = ",steer
+    
+  if steer < -0.9:
+    hobbit.writeMotors(-speed, speed)
+  elif steer < -0.1:
+    hobbit.writeMotors(0, speed)
+  elif steer > 0.9:
+    hobbit.writeMotors(speed, -speed)
+  elif steer > 0.1:
+    hobbit.writeMotors(speed, 0)
+  else:
+    hobbit.writeMotors(speed, speed)
