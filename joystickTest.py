4f6cf4b1914dd2e5f5ee0282c13bc950f23ab13e
Dillon MacEwan <dillon@mekazoic.com>
2017-03-27 16:13:25 +1100

    Create joystickTest.py

---
+++ joystickTest.py
@@ -0,0 +1,8 @@
+# this is a wifi joystck testing script
+# it used jstest-gtk for testing
+# sudo apt-get install jstest-gtk for pi
+# and jsLib https//gist.github.com/rdb/8864666
+
+import PDALib2 as hobbit
+import jsLib
+import time
