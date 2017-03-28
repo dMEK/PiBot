1f3a0dc1d07a25db4b22fe540dbebd90b19e7f83
Dillon MacEwan <dillon@mekazoic.com>
2017-03-28 20:50:49 +1100

    Create adxl345.py

---
+++ adxl345.py
@@ -0,0 +1,158 @@
+# ADXL345 Python library for Raspberry Pi 
+#
+# author:  Jonathan Williamson
+# license: BSD, see LICENSE.txt included in this package
+# 
+# This is a Raspberry Pi Python implementation to help you get started with
+# the Adafruit Triple Axis ADXL345 breakout board:
+# http://shop.pimoroni.com/products/adafruit-triple-axis-accelerometer#
+# oct.2016 - modified by William Henning, http://mikronauts.com
+#          - added simple calibration routine when running stand alone
+#          - added xoffs/yoffs/zoffs variables for calibration
+
+import smbus
+from time import sleep
+
+# select the correct i2c bus for this revision of Raspberry Pi
+revision = ([l[12:-1] for l in open('/proc/cpuinfo','r').readlines() if l[:8]=="Revision"]+['0000'])[0]
+bus = smbus.SMBus(1 if int(revision, 16) >= 4 else 0)
+
+# ADXL345 constants
+EARTH_GRAVITY_MS2   = 9.80665
+SCALE_MULTIPLIER    = 0.004
+
+DATA_FORMAT         = 0x31
+BW_RATE             = 0x2C
+POWER_CTL           = 0x2D
+
+BW_RATE_1600HZ      = 0x0F
+BW_RATE_800HZ       = 0x0E
+BW_RATE_400HZ       = 0x0D
+BW_RATE_200HZ       = 0x0C
+BW_RATE_100HZ       = 0x0B
+BW_RATE_50HZ        = 0x0A
+BW_RATE_25HZ        = 0x09
+
+RANGE_2G            = 0x00
+RANGE_4G            = 0x01
+RANGE_8G            = 0x02
+RANGE_16G           = 0x03
+
+MEASURE             = 0x08
+AXES_DATA           = 0x32
+
+xoffs = 0
+yoffs = 0
+zoffs = 0
+
+xoffs = -0.646
+yoffs = -0.532
+zoffs = 6.792 
+
+
+class ADXL345:
+
+    address = None
+
+    def __init__(self, address = 0x53):
+        self.address = address
+        self.setBandwidthRate(BW_RATE_100HZ)
+        self.setRange(RANGE_16G) #  2G / 4G don't work on Z axis
+        self.enableMeasurement()
+
+    def enableMeasurement(self):
+        bus.write_byte_data(self.address, POWER_CTL, MEASURE)
+
+    def setBandwidthRate(self, rate_flag):
+        bus.write_byte_data(self.address, BW_RATE, rate_flag)
+
+    # set the measurement range for 10-bit readings
+    def setRange(self, range_flag):
+        value = bus.read_byte_data(self.address, DATA_FORMAT)
+
+        value &= ~0x0F;
+        value |= range_flag;
+        value |= 0x08;
+
+        bus.write_byte_data(self.address, DATA_FORMAT, value)
+
+    # returns the current reading from the sensor for each axis
+    #
+    # parameter gforce:
+    #    False (default): result is returned in m/s^2
+    #    True           : result is returned in gs
+    def getAxes(self, gforce = False):
+        bytes = bus.read_i2c_block_data(self.address, AXES_DATA, 6)
+
+        x = bytes[0] | (bytes[1] << 8)
+        if(x & (1 << 16 - 1)):
+            x = x - (1<<16)
+
+        y = bytes[2] | (bytes[3] << 8)
+        if(y & (1 << 16 - 1)):
+            y = y - (1<<16)
+
+        z = bytes[4] | (bytes[5] << 8)
+        if(z & (1 << 16 - 1)):
+            z = z - (1<<16)
+
+        x = x * SCALE_MULTIPLIER
+        y = y * SCALE_MULTIPLIER
+        z = z * SCALE_MULTIPLIER
+
+        if gforce == False:
+            x = x * EARTH_GRAVITY_MS2
+            y = y * EARTH_GRAVITY_MS2
+            z = z * EARTH_GRAVITY_MS2
+
+        x = round(x, 4) - xoffs
+        y = round(y, 4) - yoffs
+        z = round(z, 4) - zoffs
+
+        return {"x": x, "y": y, "z": z}
+
+if __name__ == "__main__":
+    # if run directly we'll just create an instance of the class and output 
+    # the current readings
+    adxl345 = ADXL345()
+    
+    xmax = -30000
+    xmin =  30000
+    ymax = -30000
+    ymin =  30000
+    zmax = -30000
+    zmin =  30000
+
+    loops = 600
+
+    while (loops):       
+        loops=loops-1    
+        axes = adxl345.getAxes(True)
+        x = axes['x'] #+ 0.568; 
+        y = axes['y'] #+ 0.440;
+        z = axes['z'] #- 6.844;
+        if (x > xmax):
+           xmax = x   
+        if (x < xmin):
+           xmin = x   
+        if (y > ymax):
+           ymax = y   
+        if (y < ymin):
+           ymin = y   
+        if (z > zmax):
+           zmax = z   
+        if (z < zmin):
+           zmin = z
+
+        print "ADXL345 on address 0x%x:" % (adxl345.address)
+        print "   x = %.3fG" % ( axes['x'] )
+        print "   y = %.3fG" % ( axes['y'] )
+        print "   z = %.3fG" % ( axes['z'] )
+        sleep(0.01)
+
+    print "\nSubtract the offsets shown from the x/y/z values in adxl345.py\n"
+    print "xoffs =", (xmax+xmin)/2
+    print "yoffs =", (ymax+ymin)/2
+    print "zoffs =", (zmax-1.0)
+
+
